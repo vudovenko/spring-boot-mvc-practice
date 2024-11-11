9d01980212fd93fa20f6b3679834b82ca1c2a8d7
vudovenko <vladislav.udovenko0@yandex.ru>
2024-11-11 11:06:23 +0500

    git init

---
+++ src/main/java/dev/vudovenko/springbootmvcpractice/SpringBootMvcPracticeApplication.java
@@ -0,0 +1,13 @@
+package dev.vudovenko.springbootmvcpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vudovenko/springbootmvcpractice/SpringBootMvcPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vudovenko.springbootmvcpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
